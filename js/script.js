263fde725cbd4ca002eab2174ed6f845ec57b2fa
anuerk <anuerk@portolancs.com>
2021-12-04 17:44:01 +0100

    added possibility to contact user

--- js/script.js
+++ js/script.js
@@ -19,9 +19,6 @@ GET ​/me​/device_code initiates the GitHub device code authentication flow
 PATCH ​/me​/messages Receive new messages through websocket (wss://...)
 
 */
-
-document.cookie = "Session=test; SameSite=None; Secure";
-
 const auth_api_url = "https://chatty.1337.cx/me/device_code"
 const get_rooms_api_url = "https://chatty.1337.cx/rooms"
 const get_users_api_url = "https://chatty.1337.cx/users"
@@ -61,16 +58,14 @@ function init() {
       } else {
         console.log("log in success result: ")
         current_user = json.user
-        check_single_old_room()
+
+        leave_all_rooms()
         get_rooms()
-        enter_chat(false, true)
         get_users()
         start_user_message_socket()
       }
     })
     .catch((error) => {
-      //todo
-      document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
       console.log(error)
     });
 }
@@ -194,83 +189,66 @@ function create_user_html(user) {
 }
 
 // wird aufgerufen bei room-button click
-function enter_chat(create_new_room, init_call) {
+function enter_chat(create_new_room) {
   document.getElementById('chat_send').onclick = function () { send_message_in_room(); }
   // todo -> wenn auf user geklickt wurde
   let fetch_rooms_url = get_rooms_api_url
   let enter_fetch_url = ""
   let room = ""
 
-  if (init_call == true) {
-    room = localStorage.getItem('current_room')
-  } else {
-    // need for difference url room name
-    room = this.value
-  }
+  // need for difference url room name
+  room = this.value
 
+
+
+  //todo bug wenn in rraum wechsel hin und her und wieder zurück --> 409 confilct. muss raum zuerst verlassen!
+  console.log('current_room: ' + current_room)
+  console.log('room: ' + room)
   if (create_new_room === true) {
     enter_fetch_url = join_room_api_url + document.getElementById('new_room').value + '/users'
   } else {
     enter_fetch_url = join_room_api_url + room + '/users'
   }
 
-  let delete_fetch_url = delete_room_api_url + localStorage.getItem("current_room") + '/users'
+  let delete_fetch_url = delete_room_api_url + current_room + '/users'
 
-  //todo braucht man doch eigentlich kein local storage
-  console.log(' if condigtion')
-  console.log(localStorage.getItem("current_room"))
-  console.log(room)
-  console.log(localStorage.getItem("current_room") == room)
-  console.log(init_call)
-  if (localStorage.getItem("current_room") == room) {//&& init_call === false) {
+  if (current_room == room) {//&& init_call === false) {
     console.log("You are already in the room " + room)
     read_old_messages(room)
     // update header for current room
     let tmp = document.getElementById('chat_history').innerHTML
   }
   else {
-    // leave current room todo evtl. auslafgern als funktion
-    fetch(delete_fetch_url, {
-      method: 'delete',
+    // enter new room
+    fetch(enter_fetch_url, {
+      method: 'POST',
       credentials: "include",
     })
       .then(function (resp) {
+        current_room = room
 
-        // enter new room
-        fetch(enter_fetch_url, {
-          method: 'POST',
-          credentials: "include",
-        })
-          .then(function (resp) {
-            localStorage.setItem("current_room", room)
+        if (resp.status = 200) {
+          //2 you joined the room " + room
+          read_old_messages(room)
 
-            if (resp.status = 200) {
-              //2 you joined the room " + room
-              read_old_messages(room)
+        } else if (resp.status = 201) {
+          // todo              
+          console.log("todo 2 You created and joinend in the room " + room)
 
-            } else if (resp.status = 201) {
-              // todo              
-              console.log("todo 2 You created and joinend in the room " + room)
+        }
+        else if (resp.status = 404) {
+          // todo?
+          console.log("todo 2 You are not in the room " + room)
+        }
 
-            }
-            else if (resp.status = 404) {
-              // todo?
-              console.log("todo 2 You are not in the room " + room)
-            }
+        // update header for current room
+        let tmp = document.getElementById('chat_history').innerHTML
 
-            // update header for current room
-            let tmp = document.getElementById('chat_history').innerHTML
-
-            return
-          })
-          .catch(function (error) {
-            console.log(error)
-          });
         return
       })
       .catch(function (error) {
         console.log(error)
-      })
+      });
   }
   //todo start wss listener
 }
@@ -298,7 +276,7 @@ function send_message_in_room() {
     alert(label_need_message)
   }
   else {
-    let send_message_url = delete_room_api_url + localStorage.getItem("current_room") + '/messages'
+    let send_message_url = delete_room_api_url + current_room + '/messages'
     fetch(send_message_url, {
       method: 'POST',
       credentials: "include",
@@ -310,7 +288,7 @@ function send_message_in_room() {
       .then(function (resp) {
         if (resp.status = 409) {
           // message sent
-          read_old_messages(localStorage.getItem("current_room"))
+          read_old_messages(current_room)
         } else if (resp.status = 403) {
           alert("You are not a member of the room " + room)
         }
@@ -339,12 +317,10 @@ function read_old_messages(room) {
       for (let item in data) {
         //format_message_in_chat()
         let p = document.createElement("p")
-        if (data[item].user == localStorage.getItem('current_user')) {
-          console.log('nachricht von angemeldetem user')
+        if (data[item].user == current_user) {
           p.classList.add("from-me")
           p.innerHTML = data[item].message
         } else {
-          console.log('nachricht von jemand anderem')
           p.classList.add("from-them")
           p.innerHTML = data[item].user + ': ' + data[item].message
         }
@@ -400,117 +376,33 @@ function send_message_to_user(user) {
       .then(function (resp) {
         if (resp.status = 404) {
           alert(label_user_offline)
-        }else{
+        } else {
           alert(label_message_sent)
-        } 
-        
+        }
+
         return
       })
       .catch(function (error) {
         console.log(error)
       });
   }
-  // todo -> wenn auf user geklickt wurde
-  /*let fetch_rooms_url = get_rooms_api_url
-  let enter_fetch_url = ""
-  let room = ""
-
-  if (init_call == true) {
-    room = localStorage.getItem('current_room')
-  } else {
-    // need for difference url room name
-    room = this.value
-  }
-
-  if (create_new_room === true) {
-    enter_fetch_url = join_room_api_url + document.getElementById('new_room').value + '/users'
-  } else {
-    enter_fetch_url = join_room_api_url + room + '/users'
-  }
-
-  let delete_fetch_url = delete_room_api_url + localStorage.getItem("current_room") + '/users'
-
-  //todo braucht man doch eigentlich kein local storage
-  debugger
-  console.log(' if condigtion')
-  console.log(localStorage.getItem("current_room"))
-  console.log(room)
-  console.log(localStorage.getItem("current_room") == room)
-  console.log(init_call)
-  if (localStorage.getItem("current_room") == room) {//&& init_call === false) {
-    console.log("You are already in the room " + room)
-  }
-  else if (init_call === true) {
-    read_old_messages(room)
-    // update header for current room
-    let tmp = document.getElementById('chat_history').innerHTML
-  }
-  else {
-    // leave current room todo evtl. auslafgern als funktion
-    fetch(delete_fetch_url, {
-      method: 'delete',
-      credentials: "include",
-    })
-      .then(function (resp) {
-
-        // enter new room
-        fetch(enter_fetch_url, {
-          method: 'POST',
-          credentials: "include",
-        })
-          .then(function (resp) {
-            localStorage.setItem("current_room", room)
-
-            if (resp.status = 200) {
-              //2 you joined the room " + room
-              read_old_messages(room)
-
-            } else if (resp.status = 201) {
-              // todo              
-              console.log("todo 2 You created and joinend in the room " + room)
-
-            }
-            else if (resp.status = 404) {
-              // todo?
-              console.log("todo 2 You are not in the room " + room)
-            }
-
-            // update header for current room
-            let tmp = document.getElementById('chat_history').innerHTML
-
-            return
-          })
-          .catch(function (error) {
-            console.log(error)
-          });
-        return
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  }*/
 }
 
-function check_single_old_room() {
-  document.getElementById('users').innerHTML = '<h2>' + label_users + '</h2>'
-  console.log('url:')
-  console.log(leave_room_api_url + current_user + '/rooms')
+function leave_all_rooms() {
 
   fetch(leave_room_api_url + current_user + '/rooms', {
     credentials: "include",
   })
     .then((resp) => resp.json())
     .then(function (data) {
-      if (data.length > 1) {
-        console.log('data')
-        console.log(data)
-        // user is only allowed to be in one room
-        for (let key in data) {
-          if (temp1.hasOwnProperty(key)) {
-            leave_room(temp1[key])
-          }
+
+      console.log('data')
+      console.log(data)
+      // user is only allowed to be in one room
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+          leave_room(data[key])
         }
-        return
       }
       return
     })
